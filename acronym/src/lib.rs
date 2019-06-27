8e070322d349b33650694f31cab9f1c1a12ad4d6
rkr35 <rkr35@drexel.edu>
2019-06-27 17:31:18 -0400

    Add first implementation of Acronym

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(clippy::pedantic)]
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let mut previous = ' ';
+    let mut abbreviations = vec![];
+
+    for c in phrase.chars() {
+        if "- ".contains(previous) || c.is_uppercase() && previous.is_lowercase() {
+            abbreviations.push(c.to_ascii_uppercase());
+        }
+
+        previous = c;
+    }
+
+    abbreviations.into_iter().collect()
 }
--- acronym/tests/acronym.rs
+++ acronym/tests/acronym.rs
@@ -6,37 +6,37 @@ fn empty() {
 }
 
 #[test]
-#[ignore]
+
 fn basic() {
     assert_eq!(acronym::abbreviate("Portable Network Graphics"), "PNG");
 }
 
 #[test]
-#[ignore]
+
 fn lowercase_words() {
     assert_eq!(acronym::abbreviate("Ruby on Rails"), "ROR");
 }
 
 #[test]
-#[ignore]
+
 fn camelcase() {
     assert_eq!(acronym::abbreviate("HyperText Markup Language"), "HTML");
 }
 
 #[test]
-#[ignore]
+
 fn punctuation() {
     assert_eq!(acronym::abbreviate("First In, First Out"), "FIFO");
 }
 
 #[test]
-#[ignore]
+
 fn all_caps_words() {
     assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
 }
 
 #[test]
-#[ignore]
+
 fn non_acronym_all_caps_word() {
     assert_eq!(
         acronym::abbreviate("GNU Image Manipulation Program"),
@@ -45,7 +45,7 @@ fn non_acronym_all_caps_word() {
 }
 
 #[test]
-#[ignore]
+
 fn hyphenated() {
     assert_eq!(
         acronym::abbreviate("Complementary metal-oxide semiconductor"),
