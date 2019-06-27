b4a56a93b115ec78bce68d50a1d8bf2ddc341e46
rkr35 <rkr35@drexel.edu>
2019-06-27 12:17:30 -0400

    Add implementation for multiple nucleotide counts

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::pedantic)]
 use std::collections::HashMap;
+use std::iter::repeat;
 
 fn is_nucleotide(c: char) -> bool {
     match c {
@@ -12,14 +13,17 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
     if !is_nucleotide(nucleotide) { return Err(nucleotide); }
 
     dna.chars().try_fold(0, |count, c| {
-        if is_nucleotide(c) { Ok( count + usize::from(c == nucleotide) ) }
+        if is_nucleotide(c) { Ok(count + usize::from(c == nucleotide)) } 
         else { Err(c) }
     })
 }
 
-pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+type Counts = HashMap<char, usize>;
+pub fn nucleotide_counts(dna: &str) -> Result<Counts, char> {
+    let counts = ['A', 'C', 'G', 'T'].iter().cloned().zip(repeat(0)).collect();
+
+    dna.chars().try_fold(counts, |mut counts : Counts, c| { 
+        if is_nucleotide(c) { Ok({ *counts.get_mut(&c).unwrap() += 1; counts }) }
+        else { Err(c) }
+    })
 }
