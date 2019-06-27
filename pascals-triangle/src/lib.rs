fb0a169c9ea99742539909daffcfecca58d00419
rkr35 <rkr35@drexel.edu>
2019-06-26 20:20:18 -0400

    Implement Pascal's Triangle

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,14 +1,31 @@
 type Row = Vec<u32>;
 type Rows = Vec<Row>;
 
+// todo: Revisit when `const fn` lands in stable.
+fn falling_factorial(n: usize, k: usize) -> usize {
+    ((n-k+1) ..= n).product()
+}
+
+fn binomial_coefficient(n: usize, k: usize) -> usize {
+    let non_repeating_permutations = falling_factorial(n, k);
+    let combinations_per_permutation: usize = (1 ..= k).product();
+    non_repeating_permutations / combinations_per_permutation
+}
+
 pub struct PascalsTriangle {
     rows: Rows,
+}
+
+fn get_row(row: usize) -> Row {
+    (0 ..= row)
+        .map(|column| binomial_coefficient(row, column) as u32)
+        .collect()
 }
 
 impl PascalsTriangle {
     pub fn new(number_of_rows: usize) -> Self {
         Self {
-            rows: Vec::with_capacity(number_of_rows),
+            rows: (0 .. number_of_rows).map(get_row).collect(),
         }
     }
 
