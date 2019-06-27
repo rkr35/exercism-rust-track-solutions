55e8819c1c25797b4cb7a2bca96264eda7893e43
rkr35 <rkr35@drexel.edu>
2019-06-27 15:37:58 -0400

    Use zip() instead of manually creating tuples ourselves

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
+use std::iter::repeat;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    h.iter()
-        .flat_map(|(&points, characters)| characters.iter().map(move |c| (c.to_ascii_lowercase(), points)))
+    h
+        .iter()
+        .flat_map(|(&points, characters)| characters.iter().map(char::to_ascii_lowercase).zip(repeat(points)))
         .collect()
 }
