a3d834d400ac576d8b483550b6df4b3dcf82e6b5
rkr35 <rkr35@drexel.edu>
2019-06-27 15:20:04 -0400

    Add first implementation for ETL

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,5 +1,13 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    let mut new = BTreeMap::new();
+
+    for (&points, characters) in h.iter() {
+        for c in characters {
+            new.insert(c.to_lowercase().next().unwrap(), points);
+        }        
+    }
+
+    new
 }
