323426529e5343ccc774bcc36d2a6303432b56d0
rkr35 <rkr35@drexel.edu>
2019-06-27 20:20:34 -0400

    Add even shorter implementation for Sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -3,9 +3,10 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut numbers: Vec<_> = (2..=upper_bound).map(Option::from).collect();
     (0..numbers.len())
-        .filter_map(|i| numbers[i].map(|prime| {
-            (prime..=upper_bound).step_by(prime as usize).skip(1).for_each(|i| numbers[(i - 2) as usize] = None); 
-            prime
-        }))
+        .filter_map(|i| {
+            let prime = numbers[i].take()?;
+            numbers.iter_mut().skip(i).step_by(prime as usize).skip(1).for_each(|n| *n = None);
+            Some(prime)
+        })
         .collect()
 }
