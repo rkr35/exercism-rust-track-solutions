607e4f34c5df85f5de795ab0858e7e9c5ce042af
rkr35 <rkr35@drexel.edu>
2019-06-28 17:46:38 -0400

    Generify number_sides_equal() over number of side lengths
    This pair-wise comparison functionality may be useful for other exercises
    down the road, so I decided to abstract it over
    any non-negative integer number of sides.

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -3,13 +3,15 @@
 use std::iter::{empty, Sum};
 use std::ops::Add;
 
-pub struct Triangle<T>([T; 3]);
+const NUM_SIDES: usize = 3;
+
+pub struct Triangle<T>([T; NUM_SIDES]);
 
 impl<T> Triangle<T>
 where
     T: Copy + Add + Sum<T> + PartialOrd<T> + PartialOrd<<T as Add>::Output> 
 {
-    pub fn build(sides: [T; 3]) -> Option<Self> {
+    pub fn build(sides: [T; NUM_SIDES]) -> Option<Self> {
         let sum: T = sides.iter().cloned().sum();
         let &max = sides.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
 
@@ -21,27 +23,15 @@ where
     }
 
     fn number_sides_equal(&self) -> usize {
-        let [side1, side2, side3] = self.0;
+        let last_index = self.0.len() - 1;
 
-        let mut num = 0;
-
-        if side1 == side2 {
-            num += 1;
-        }
-
-        if side1 == side3 {
-            num += 1;
-        }
-
-        if side2 == side3 {
-            num += 1;
-        }
-
-        num
+        (0..last_index)
+            .map(|i| (i+1..=last_index).map(|j| usize::from(self.0[i] == self.0[j])).sum::<usize>())
+            .sum()
     }
 
     pub fn is_equilateral(&self) -> bool {
-        self.number_sides_equal() == 3
+        self.number_sides_equal() == NUM_SIDES
     }
 
     pub fn is_isosceles(&self) -> bool {
