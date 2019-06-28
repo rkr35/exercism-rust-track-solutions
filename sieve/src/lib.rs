0179a52ff1f84622077b5e096ede6d0f2151a0c5
rkr35 <rkr35@drexel.edu>
2019-06-27 20:14:30 -0400

    Add shorter implementation for Sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,22 +1,11 @@
 #![warn(clippy::pedantic)]
 
-type MarkableNumber = Option<u64>;
-
-fn mark_multiples(numbers: &mut [MarkableNumber], prime: u64) -> u64 {
-    let length = numbers.len();
-
-    (2..)
-        .map(|i| (prime * i - 2) as usize)
-        .take_while(|&i| i < length)
-        .for_each(|i| numbers[i] = None );
-
-    prime
-}
-
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut numbers: Vec<_> = (2..=upper_bound).map(MarkableNumber::from).collect();
-
+    let mut numbers: Vec<_> = (2..=upper_bound).map(Option::from).collect();
     (0..numbers.len())
-        .filter_map(|i| numbers[i].map(|prime| mark_multiples(&mut numbers, prime)))
+        .filter_map(|i| numbers[i].map(|prime| {
+            (prime..=upper_bound).step_by(prime as usize).skip(1).for_each(|i| numbers[(i - 2) as usize] = None); 
+            prime
+        }))
         .collect()
 }
