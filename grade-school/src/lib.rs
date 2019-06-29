fca8984ef4522d1d4723042f90c9aaf8f9d6fbbf
rkr35 <rkr35@drexel.edu>
2019-06-28 20:29:19 -0400

    Use BTreeMap and BTreeSet for sorted values

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,24 +1,25 @@
 #![warn(clippy::pedantic)]
 
+use std::collections::{BTreeMap, BTreeSet};
+
 type Grade = u32;
-type Students = Vec<String>;
+type Students = BTreeSet<String>;
 
 #[derive(Default)]
-pub struct School(std::collections::HashMap<Grade, Students>);
+pub struct School(BTreeMap<Grade, Students>);
 
 impl School {
     pub fn new() -> Self { Self::default() }
 
     pub fn add(&mut self, grade: Grade, student: &str) {
-        self.0.entry(grade).or_default().push(student.to_string());
+        self.0.entry(grade).or_default().insert(student.to_string());
     }
 
     pub fn grades(&self) -> Vec<Grade> {
-        let mut g: Vec<_> = self.0.keys().cloned().collect(); g.sort_unstable();
-        g
+        self.0.keys().cloned().collect()
     }
 
-    pub fn grade(&self, grade: Grade) -> Option<Students> {
-        self.0.get(&grade).cloned().map(|mut s| { s.sort_unstable(); s })
+    pub fn grade(&self, grade: Grade) -> Option<Vec<String>> {
+        self.0.get(&grade).map(|students| students.iter().cloned().collect())
     }
 }
