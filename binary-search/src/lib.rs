f65f26c9e62b4a68da14cad2738c9a242d53a9fe
rkr35 <rkr35@drexel.edu>
2019-06-28 21:11:18 -0400

    Add first implementation of Binary Search (w/o generics)

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,23 @@
+use std::cmp::Ordering;
+use std::mem::replace;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.is_empty() {
+        return None;
+    }
+
+    let (mut low, mut high) = (0, array.len());
+    let mut middle = (low + high) / 2;
+
+    loop {
+        match key.cmp(&array[middle]) {
+            Ordering::Equal => return Some(middle),
+            Ordering::Less => high = middle,
+            Ordering::Greater => low = middle,
+        };
+
+        if replace(&mut middle, (low + high) / 2) == middle {
+            return None;
+        }
+    }
 }
