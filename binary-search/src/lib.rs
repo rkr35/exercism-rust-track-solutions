750768080ca343e7385a6a0b9179a71fc6fdb65a
rkr35 <rkr35@drexel.edu>
2019-06-28 21:13:04 -0400

    Use Option unpack shorthand to exit on empty array

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -2,15 +2,11 @@ use std::cmp::Ordering;
 use std::mem::replace;
 
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    if array.is_empty() {
-        return None;
-    }
-
     let (mut low, mut high) = (0, array.len());
     let mut middle = (low + high) / 2;
 
     loop {
-        match key.cmp(&array[middle]) {
+        match key.cmp(array.get(middle)?) {
             Ordering::Equal => return Some(middle),
             Ordering::Less => high = middle,
             Ordering::Greater => low = middle,
