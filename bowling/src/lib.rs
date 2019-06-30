e93436cbacc64aa367a3bbd4d31baa986e6e2a8d
rkr35 <rkr35@drexel.edu>
2019-06-30 17:36:58 -0400

    Add implementation for first seven tests

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,18 +4,43 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+pub struct BowlingGame {
+    pins_left: u16,
+    rolls_left: usize,
+}
+
+impl Default for BowlingGame {
+    fn default() -> Self {
+        Self {
+            pins_left: 10,
+            rolls_left: 20
+        }
+    }
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        Self::default()
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if pins > self.pins_left {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+
+        if self.rolls_left == 0 {
+            return Err(Error::GameComplete);
+        }
+
+        self.rolls_left -= 1;
+
+        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        match self.rolls_left {
+            0 => Some(0),
+            _ => None,
+        }
     }
 }
