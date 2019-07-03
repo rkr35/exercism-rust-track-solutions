278714b966892b4d0e51f835c9700d6f659a68cf
rkr35 <rkr35@drexel.edu>
2019-07-03 17:03:43 -0400

    Add implementation for all tests

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -1,8 +1,6 @@
 #![warn(clippy::pedantic)]
-use std::collections::HashMap;
 use std::cmp::Ordering;
-
-type Name = String;
+use std::collections::HashMap;
 
 #[derive(Default)]
 struct Stats {
@@ -30,14 +28,21 @@ impl Stats {
 
             "loss" => self.loss += 1,
 
-            _ => unimplemented!("update() not implemented for conclusion of \"{}\"", conclusion)
+            _ => unimplemented!(
+                "update() not implemented for conclusion of \"{}\"",
+                conclusion
+            ),
         };
 
         self.matches += 1;
     }
 }
 
-macro_rules! row_format {($($arg:expr),+) => {format!("{:31}|{:>width$} |{:>width$} |{:>width$} |{:>width$} |{:>width$}", $($arg,)+ width=3)};}
+macro_rules! row_format {
+    ($($arg:expr),+) => {
+        format!("{:31}|{:>width$} |{:>width$} |{:>width$} |{:>width$} |{:>width$}", $($arg,)+ width=3)
+    };
+}
 
 fn get_header() -> String {
     row_format!("Team", "MP", "W", "D", "L", "P")
@@ -48,22 +53,53 @@ pub fn tally(match_results: &str) -> String {
         return get_header();
     }
 
-    let mut team_stats: HashMap<Name, Stats> = HashMap::new();
-    let split: Vec<_> = match_results.split(';').collect();
-    
-    let first_team = split[0];
-    let second_team = split[1];
-    let conclusion = split[2];
+    let mut pieces = match_results.split(';').flat_map(|s| s.split('\n'));
 
-    let opposite_conclusion = match conclusion {
-        "win" => "loss",
-        "loss" => "win",
-        "draw" => "draw",
-        _ => unreachable!("Encountered unknown conclusion when finding opposite (\"{}\")", conclusion)
-    };
+    let mut line: [Option<&str>; 3] = Default::default();
+    let mut line_cursor = 0;
 
-    team_stats.entry(first_team.to_string()).or_default().update(conclusion);
-    team_stats.entry(second_team.to_string()).or_default().update(opposite_conclusion);
+    let mut team_stats: HashMap<_, Stats> = HashMap::new();
+
+    while let Some(piece) = pieces.next() {
+        line[line_cursor] = Some(piece);
+        line_cursor += 1;
+
+        if line_cursor != line.len() {
+            continue;
+        }
+
+        let [first_team, second_team, conclusion] = line;
+
+        let [first_team, second_team, conclusion] = [
+            first_team.unwrap(),
+            second_team.unwrap(),
+            conclusion.unwrap(),
+        ];
+
+        let opposite_conclusion = match conclusion {
+            "win" => "loss",
+            "loss" => "win",
+            "draw" => "draw",
+
+            _ => unreachable!(
+                "Encountered unknown conclusion when finding opposite (\"{}\")",
+                conclusion
+            ),
+        };
+
+        team_stats
+            .entry(first_team)
+            .or_default()
+            .update(conclusion);
+
+        team_stats
+            .entry(second_team)
+            .or_default()
+            .update(opposite_conclusion);
+
+        line = Default::default();
+        line_cursor = 0;
+    }
 
     let mut team_stats: Vec<_> = team_stats.into_iter().collect();
 
@@ -74,11 +110,20 @@ pub fn tally(match_results: &str) -> String {
         }
     });
 
-    get_header() + "\n" + 
-    
-    &team_stats
-        .into_iter()
-        .map(|(name, stats)| row_format!(name, stats.matches, stats.won, stats.drew, stats.loss, stats.points))
-        .collect::<Vec<_>>()
-        .join("\n")
+    get_header()
+        + "\n"
+        + &team_stats
+            .into_iter()
+            .map(|(name, stats)| {
+                row_format!(
+                    name,
+                    stats.matches,
+                    stats.won,
+                    stats.drew,
+                    stats.loss,
+                    stats.points
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
 }
--- tournament/tests/tournament.rs
+++ tournament/tests/tournament.rs
@@ -20,7 +20,6 @@ fn a_win_is_three_points_a_loss_is_zero_points() {
 }
 
 #[test]
-#[ignore]
 fn a_win_can_also_be_expressed_as_a_loss() {
     let input = "Blithering Badgers;Allegoric Alaskans;loss";
     let expected = "".to_string()
@@ -32,7 +31,6 @@ fn a_win_can_also_be_expressed_as_a_loss() {
 }
 
 #[test]
-#[ignore]
 fn a_different_team_can_win() {
     let input = "Blithering Badgers;Allegoric Alaskans;win";
     let expected = "".to_string()
@@ -44,7 +42,6 @@ fn a_different_team_can_win() {
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_one_match() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -57,7 +54,6 @@ fn there_can_be_more_than_one_match() {
 }
 
 #[test]
-#[ignore]
 fn a_draw_is_one_point_each() {
     let input = "Allegoric Alaskans;Blithering Badgers;draw\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -70,7 +66,6 @@ fn a_draw_is_one_point_each() {
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_one_winner() {
     let input = "Allegoric Alaskans;Blithering Badgers;loss\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -83,7 +78,6 @@ fn there_can_be_more_than_one_winner() {
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_two_teams() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Blithering Badgers;Courageous Californians;win\n"
@@ -98,7 +92,6 @@ fn there_can_be_more_than_two_teams() {
 }
 
 #[test]
-#[ignore]
 fn typical_input() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Devastating Donkeys;Courageous Californians;draw\n"
@@ -117,7 +110,6 @@ fn typical_input() {
 }
 
 #[test]
-#[ignore]
 fn incomplete_competition_not_all_pairs_have_played() {
     let input = "Allegoric Alaskans;Blithering Badgers;loss\n".to_string()
         + "Devastating Donkeys;Allegoric Alaskans;loss\n"
@@ -134,7 +126,6 @@ fn incomplete_competition_not_all_pairs_have_played() {
 }
 
 #[test]
-#[ignore]
 fn ties_broken_alphabetically() {
     let input = "Courageous Californians;Devastating Donkeys;win\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win\n"
