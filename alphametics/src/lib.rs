ed5945b4a1ce34633abaef166950da3ea838aca6
rkr35 <rkr35@drexel.edu>
2019-07-07 18:22:10 -0400

    Add implementation for solving the general case

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -1,78 +1,87 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
-use std::collections::{HashMap};
-use std::convert::TryFrom;
+use std::collections::{HashMap, HashSet};
+use std::iter::once;
 
 macro_rules! get_digits {
     ($number:expr, $num_digits:expr) => {
-        (0..$num_digits).map(|digit_index| $number / 10_usize.pow(digit_index as u32) % 10)
+        (0..$num_digits)
+            .map(move |digit_index| $number / 10_usize.pow(digit_index as u32) % 10)
+            .rev()
     };
 }
 
-fn has_unique_digits(number_to_check: usize,
-                     number_of_digits_in_number_to_check: usize) -> bool {
-    
+fn test_permutation(letters: &[(char, isize)], permutation: usize, first_letters: &HashSet<char>) -> Option<HashMap<char, u8>> {
     let mut seen_digits = [false; 10];
+    let mut letter_digit_pairs: [(char, u8); 10] = [Default::default(); 10];
+    let mut cursor = 0;
 
-    get_digits!(number_to_check, number_of_digits_in_number_to_check)
-        .all(|digit| {
-            let already_seen = seen_digits[digit];
-            seen_digits[digit] = true;
-            !already_seen
-        })
-}
+    let mut solution_value = 0;
 
-fn panic_on_digit_length_greater_than_base(k: usize) {
-    if k > 10 {
-        panic!("0 <= k <= 10. Passed in k = {}", k);
+    for (i, digit) in get_digits!(permutation, letters.len()).enumerate() {
+        let (letter, constant) = letters[i];
+
+        if seen_digits[digit] || digit == 0 && first_letters.contains(&letter) {
+            return None;
+        }
+
+        seen_digits[digit] = true;
+        solution_value += constant * digit as isize;
+        letter_digit_pairs[cursor] = (letter, digit as u8);
+        cursor += 1;
+    }
+
+    if solution_value == 0 {
+        Some(letter_digit_pairs[0..cursor].iter().copied().collect())
+    } else {
+        None
     }
 }
 
-fn get_smallest_permutation(k: usize) -> usize {
-    panic_on_digit_length_greater_than_base(k);
-    (0..k).fold(0, |acc, current| acc * 10 + current)
-}
-
-fn get_largest_permutation(k: usize) -> usize {
-    panic_on_digit_length_greater_than_base(k);
-    (0..k).fold(0, |acc, current| acc * 10 + 9 - current)
+fn find_solution(letters: &[(char, isize)], first_letters: &HashSet<char>) -> Option<HashMap<char, u8>> {
+    (
+        (0..letters.len()).fold(0, |acc, current| acc * 10 + current) 
+        ..=
+        (0..letters.len()).fold(0, |acc, current| acc * 10 + 9 - current)
+    )
+    
+    .find_map(|permutation| test_permutation(&letters, permutation, first_letters))
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let mut operands: Vec<_> = input.split(" + ").collect();
-    let last_two: Vec<_> = operands.last()?.split(" == ").take(2).collect();
+    let last_two: Vec<_> = operands.last()?.split(" == ").collect();
     *operands.last_mut()? = last_two.get(0)?;
     let result = last_two.get(1)?;
 
-    let mut constants: HashMap<char, isize> = HashMap::new();
+    let constants: Vec<_> = {
+        let mut constants = HashMap::new();
 
-    for operand in operands {
-        for (i, letter) in operand.chars().rev().enumerate() {
-            let exponent = u32::try_from(i).expect("usize exponent could not fit into u32.");
-            *constants.entry(letter).or_default() += 10_isize.pow(exponent);
-        }
-    }
+        let mut add_to_constants = |word: &str, direction: isize| {
+            for (i, letter) in word.chars().rev().enumerate() {
+                *constants.entry(letter).or_default() += direction * 10_isize.pow(i as u32);
+            }
+        };
 
-    for (i, letter) in result.chars().rev().enumerate() {
-        let exponent = u32::try_from(i).expect("usize exponent could not fit into u32.");
-        *constants.entry(letter).or_default() -= 10_isize.pow(exponent);
-    }
-
-    dbg!(&constants);
-    dbg!(&result);
-
-    let letters: Vec<_> = constants.keys().collect();
-    let num_letters = letters.len();
-    let smallest_permutation = get_smallest_permutation(num_letters);
-    let largest_permutation = get_largest_permutation(num_letters);
-
-    for permutation in smallest_permutation..=largest_permutation {
-        if !has_unique_digits(permutation, num_letters) {
-            continue;
+        for operand in &operands {
+            add_to_constants(operand, 1)
         }
 
-        let digits = get_digits!(permutation, num_letters);
-    }
-    None
+        add_to_constants(result, -1);
+
+        constants.into_iter().collect()
+    };
+
+    let first_letters = operands
+        .iter()
+        .chain(once(result))
+        .map(|word| {
+            word.chars()
+                .nth(0)
+                .expect("could not obtain first character of a word")
+        })
+        .collect();
+
+    find_solution(&constants, &first_letters)
 }
