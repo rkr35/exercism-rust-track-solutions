926f2c7f847e717b3f46ab068d627ba352bf7823
rkr35 <rkr35@drexel.edu>
2019-07-11 09:52:46 -0400

    Add implementation for all test cases except special case

--- two-bucket/src/lib.rs
+++ two-bucket/src/lib.rs
@@ -18,11 +18,85 @@ pub struct BucketStats {
 
 /// Solve the bucket problem
 pub fn solve(capacity_1: u8, capacity_2: u8, goal: u8, start_bucket: &Bucket) -> BucketStats {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+    type PublicBucket = crate::Bucket;
+
+    #[derive(Debug)]
+    struct Bucket {
+        filled: u8,
+        capacity: u8,
+        bucket: PublicBucket,
+    }
+
+    impl Bucket {
+        fn new(bucket: PublicBucket, capacity: u8) -> Self {
+            Self {
+                filled: 0,
+                capacity,
+                bucket,
+            }
+        }
+
+        fn fill(&mut self) {
+            self.filled = self.capacity;
+        }
+
+        fn is_full(&self) -> bool {
+            self.filled == self.capacity
+        }
+
+        fn empty(&mut self) {
+            self.filled = 0;
+        }
+
+        fn is_empty(&self) -> bool {
+            self.filled == 0
+        }
+
+        fn r#move(&mut self, amount_from_me: u8, destination_bucket: &mut Bucket) {
+            self.filled -= amount_from_me;
+            destination_bucket.filled += amount_from_me;
+        }
+    }
+    
+    let [mut initial_bucket, mut other_bucket] = {
+        use crate::Bucket::*;
+        match start_bucket {
+            One => [Bucket::new(One, capacity_1), Bucket::new(Two, capacity_2)],
+            Two => [Bucket::new(Two, capacity_2), Bucket::new(One, capacity_1)],
+        }
+    };
+    
+    let is_filled_to_goal = |bucket: &Bucket| bucket.filled == goal;
+    let mut moves = 0;
+
+    loop {
+        if initial_bucket.is_empty() {
+            moves += 1;
+            initial_bucket.fill();
+        }
+
+        if is_filled_to_goal(&initial_bucket) {
+            return BucketStats {
+                moves,
+                goal_bucket: initial_bucket.bucket,
+                other_bucket: other_bucket.filled
+            };
+        } else if is_filled_to_goal(&other_bucket) {
+            return BucketStats {
+                moves,
+                goal_bucket: other_bucket.bucket,
+                other_bucket: initial_bucket.filled
+            };
+        } 
+
+        if other_bucket.is_full() {
+            moves += 1;
+            other_bucket.empty();
+        }
+
+        let how_much_left_to_fill_other_bucket = other_bucket.capacity - other_bucket.filled;
+        let how_much_to_move_from_initial_to_other_bucket = initial_bucket.filled.min(how_much_left_to_fill_other_bucket);
+        initial_bucket.r#move(how_much_to_move_from_initial_to_other_bucket, &mut other_bucket);
+        moves += 1;
+    }
 }
