f3f024b2e96cc45c703d93164612a30b3ffdb80e
rkr35 <rkr35@drexel.edu>
2019-07-12 16:18:30 -0400

    Use faster is_palindrome check

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -6,22 +6,19 @@ pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
     use rayon::prelude::*;
 
     fn is_palindrome(number: u64) -> bool {
-        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-        let number_of_digits = 1 + (number as f64).log10().floor() as u64;
+        let mut reversed = 0;
+        let mut number_copy = number;
 
-        let get_digits_iterator = || (0..number_of_digits)
-                                        .map(|digit_index| number / 10_u64.pow(digit_index as u32) % 10);
+        // While there are digits to extract
+        while number_copy > 0 {
+            // Add the next digit to the reconstructed number.
+            reversed = reversed * 10 + number_copy % 10;
 
-        let number_of_digits_per_iterator = (number_of_digits / 2) as usize;
+            // Slide the number right one digit.
+            number_copy /= 10;
+        }
 
-        let digits_forward = get_digits_iterator()
-                                .take(number_of_digits_per_iterator);
-        
-        let digits_backward = get_digits_iterator()
-                                .rev()
-                                .take(number_of_digits_per_iterator);
-        
-        digits_forward.eq(digits_backward)
+        reversed == number
     }
 
     (min..=max)
