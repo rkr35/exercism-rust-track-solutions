7493b3153ec91c82360b8a72c6e552c4a90ec97a
rkr35 <rkr35@drexel.edu>
2019-07-12 15:43:27 -0400

    Silence some clippy false positives

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,12 +1,16 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
 pub type Palindrome = u64;
 pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
     use rayon::prelude::*;
 
     fn is_palindrome(number: u64) -> bool {
+        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
         let number_of_digits = 1 + (number as f64).log10().floor() as u64;
 
         let get_digits_iterator = || (0..number_of_digits)
-                                        .map(|digit_index| number / 10u64.pow(digit_index as u32) % 10);
+                                        .map(|digit_index| number / 10_u64.pow(digit_index as u32) % 10);
 
         let number_of_digits_per_iterator = (number_of_digits / 2) as usize;
 
