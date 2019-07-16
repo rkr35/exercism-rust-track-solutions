8b8dd55c53ede34c61e33daa23d9d6726b65a5df
rkr35 <rkr35@drexel.edu>
2019-07-16 11:24:46 -0400

    Rewrite write_digits without intermediary floats

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,47 +1,60 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
-const MAX_HEAP_ALLOC_BYTES: usize = 16;
-
 #[derive(Debug, Default)]
 struct Run {
     character: u8,
     count: usize
 }
 
+fn write_digits(destination: &mut String, mut number: usize) {
+    let (digits, num_free_slots_left) = {
+        const MAX_NUMBER_OF_DIGITS: usize = 2;
+        let mut buffer = [0; MAX_NUMBER_OF_DIGITS];
+        let mut num_added = 0;
+
+        while number > 0 {
+            let slot = buffer.get_mut(num_added).expect("Exceeded digits upper-bound");
+            *slot = number % 10;
+            num_added += 1;
+            number /= 10;
+        }
+
+        if destination.len() + num_added > destination.capacity() {
+            panic!("Writing {} digits will cause the {}/{} byte buffer to reallocate.",
+                num_added, destination.len(), destination.capacity());
+        }
+
+        (buffer, buffer.len() - num_added)
+    };
+
+    for &digit in digits.iter().rev().skip(num_free_slots_left) {
+        destination.push_str(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][digit]);
+    }
+}
+
 fn write(destination: &mut String, source: &Run) {
-    macro_rules! panic_if_reallocates {
-        ($num_bytes:expr) => {
-            if destination.len() + $num_bytes > destination.capacity() {
-                panic!("write(): writing {} bytes will cause a reallocation to the original buffer of {}/{} bytes",
-                    $num_bytes, destination.len(), destination.capacity());
-            }
-        };
-    }
-
     if source.count > 1 {
-        let num_digits = 1 + (source.count as f64)
-            .log10()
-            .floor() as usize;
-
-        panic_if_reallocates!(num_digits);
-
-        (0..num_digits)
-            .map(|i| source.count / 10_usize.pow(i as u32) % 10)
-            .map(|digit| ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][digit])
-            .rev()
-            .for_each(|digit| destination.push_str(digit));
+        write_digits(destination, source.count);
+    }
+    
+    if destination.len() + 1 > destination.capacity() {
+        panic!("Writing one more letter will cause the {} byte buffer to reallocate.",
+            destination.capacity());
     }
 
-    panic_if_reallocates!(1);
+    let character = [source.character];
+    let character = std::str::from_utf8(&character)
+        .expect("Error converting a single u8 Run character into &str");
 
-    destination
-        .push_str(std::str::from_utf8(&[source.character])
-        .expect("Error converting a single u8 Run character into &str"));
+    destination.push_str(character);
 }
 
 pub fn encode(source: &str) -> String {
-    let current = source.bytes().nth(0).map(|character| Run { character, count: 1 });
+    const MAX_HEAP_ALLOC_BYTES: usize = 14;
+
+    let mut bytes = source.bytes();
+    let current = bytes.nth(0).map(|character| Run { character, count: 1 });
 
     if current.is_none() {
         return String::new();
@@ -50,7 +63,7 @@ pub fn encode(source: &str) -> String {
     let mut current = current.expect("Tried to unwrap empty current Run");
     let mut encoded = String::with_capacity(MAX_HEAP_ALLOC_BYTES);
 
-    for character in source.bytes().skip(1) {
+    for character in bytes {
         if current.character == character {
             current.count += 1;
         } else {
