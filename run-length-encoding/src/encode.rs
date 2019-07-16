d08d9ba17a2de34f75093f35887e7874442a3f50
rkr35 <rkr35@drexel.edu>
2019-07-16 13:44:17 -0400

    Add error-handling

---
+++ run-length-encoding/src/encode.rs
@@ -0,0 +1,153 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
+#[derive(Debug, Default)]
+struct Run {
+    character: u8,
+    count: usize
+}
+
+#[derive(Debug)]
+pub struct ReallocateError {
+    capacity: usize,
+    current: usize,
+    planned_to_add: usize
+}
+
+#[derive(Debug)]
+pub enum Error {
+    NonAsciiCharacter { 
+        character: u8, 
+        utf8_error: std::str::Utf8Error 
+    },
+
+    DigitsReallocates(ReallocateError),
+    
+    CharacterReallocates {
+        character: u8,
+        reallocate_error: ReallocateError
+    },
+
+    DigitsExceededUpperBound {
+        whats_left: usize,
+        num_added: usize,
+        upper_bound: usize,
+    },
+
+    FinalResultReallocated {
+        intended_final_capacity: usize, 
+        actual_final_capacity: usize
+    },
+}
+
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{:#?}", self)
+    }
+}
+
+const DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+fn digits(destination: &mut String, mut number: usize) -> Result<(), Error> {
+    let (digits, num_free_slots_left) = {
+        const MAX_NUMBER_OF_DIGITS: usize = 2;
+        let mut buffer = [0; MAX_NUMBER_OF_DIGITS];
+        let mut num_added = 0;
+
+        while number > 0 {
+            let slot = buffer
+                .get_mut(num_added)
+                .ok_or(Error::DigitsExceededUpperBound {
+                    whats_left: number,
+                    num_added,
+                    upper_bound: MAX_NUMBER_OF_DIGITS
+                })?;
+
+            *slot = number % 10;
+            num_added += 1;
+            number /= 10;
+        }
+
+        if destination.len() + num_added > destination.capacity() {
+            let error = Error::DigitsReallocates(ReallocateError { 
+                capacity: destination.capacity(),
+                current: destination.len(),
+                planned_to_add: num_added
+            });
+
+            return Err(error);
+        }
+
+        (buffer, buffer.len() - num_added)
+    };
+
+    for &digit in digits.iter().rev().skip(num_free_slots_left) {
+        destination.push_str(DIGITS[digit]);
+    }
+
+    Ok(())
+}
+
+fn to(destination: &mut String, source: &Run) -> Result<(), Error> {
+    if source.count > 1 {
+        digits(destination, source.count)?;
+    }
+    
+    if destination.len() + 1 > destination.capacity() {
+        let error = Error::CharacterReallocates {
+            character: source.character,
+            reallocate_error: ReallocateError { 
+                capacity: destination.capacity(),
+                current: destination.len(),
+                planned_to_add: 1
+            }
+        };
+        
+        return Err(error);
+    }
+
+    let character = [source.character];
+    let character = std::str::from_utf8(&character)
+        .map_err(|utf8_error| Error::NonAsciiCharacter {
+            character: source.character,
+            utf8_error
+        })?;
+
+    destination.push_str(character);
+
+    Ok(())
+}
+
+pub fn up_to_specificed_heap_bytes(source: &str, max_heap_alloc_bytes: usize) -> Result<String, Error> {
+    let mut bytes = source.bytes();
+    let current = bytes.nth(0).map(|character| Run { character, count: 1 });
+
+    if current.is_none() {
+        return Ok(String::new());
+    }
+
+    let mut current = current.expect("Tried to unwrap empty current Run");
+    let mut encoded = String::with_capacity(max_heap_alloc_bytes);
+
+    for character in bytes {
+        if current.character == character {
+            current.count += 1;
+        } else {
+            to(&mut encoded, &current)?;
+            current = Run {character, count: 1};
+        }
+    }
+
+    to(&mut encoded, &current)?;
+
+    if encoded.capacity() <= max_heap_alloc_bytes {
+        Ok(encoded)
+    } else {
+        Err(Error::FinalResultReallocated {
+            intended_final_capacity: max_heap_alloc_bytes,
+            actual_final_capacity: encoded.capacity()
+        })
+    }
+}
--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,86 +1,14 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
-#[derive(Debug, Default)]
-struct Run {
-    character: u8,
-    count: usize
-}
-
-const DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-fn write_digits(destination: &mut String, mut number: usize) {
-    let (digits, num_free_slots_left) = {
-        const MAX_NUMBER_OF_DIGITS: usize = 2;
-        let mut buffer = [0; MAX_NUMBER_OF_DIGITS];
-        let mut num_added = 0;
-
-        while number > 0 {
-            let slot = buffer.get_mut(num_added).expect("Exceeded digits upper-bound");
-            *slot = number % 10;
-            num_added += 1;
-            number /= 10;
-        }
-
-        if destination.len() + num_added > destination.capacity() {
-            panic!("Writing {} digits will cause the {}/{} byte buffer to reallocate.",
-                num_added, destination.len(), destination.capacity());
-        }
-
-        (buffer, buffer.len() - num_added)
-    };
-
-    for &digit in digits.iter().rev().skip(num_free_slots_left) {
-        destination.push_str(DIGITS[digit]);
-    }
-}
-
-fn write(destination: &mut String, source: &Run) {
-    if source.count > 1 {
-        write_digits(destination, source.count);
-    }
-    
-    if destination.len() + 1 > destination.capacity() {
-        panic!("Writing one more letter will cause the {} byte buffer to reallocate.",
-            destination.capacity());
-    }
-
-    let character = [source.character];
-    let character = std::str::from_utf8(&character)
-        .expect("Error converting a single u8 Run character into &str");
-
-    destination.push_str(character);
-}
+mod encode;
 
 pub fn encode(source: &str) -> String {
     const MAX_HEAP_ALLOC_BYTES: usize = 13;
-
-    let mut bytes = source.bytes();
-    let current = bytes.nth(0).map(|character| Run { character, count: 1 });
-
-    if current.is_none() {
-        return String::new();
+    match encode::up_to_specificed_heap_bytes(source, MAX_HEAP_ALLOC_BYTES) {
+        Ok(encoded) => encoded,
+        Err(e) => panic!("{}", e)
     }
-
-    let mut current = current.expect("Tried to unwrap empty current Run");
-    let mut encoded = String::with_capacity(MAX_HEAP_ALLOC_BYTES);
-
-    for character in bytes {
-        if current.character == character {
-            current.count += 1;
-        } else {
-            write(&mut encoded, &current);
-            current = Run {character, count: 1};
-        }
-    }
-
-    write(&mut encoded, &current);
-
-    if encoded.capacity() != MAX_HEAP_ALLOC_BYTES {
-        panic!("`encoded` reallocated from {} bytes to {} bytes", MAX_HEAP_ALLOC_BYTES, encoded.capacity());
-    }
-
-    encoded
 }
 
 pub fn decode(source: &str) -> String {
