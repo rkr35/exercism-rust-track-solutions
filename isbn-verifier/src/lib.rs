4c619c55f04c0688741c83dedcbe3c74789c2393
rkr35 <rkr35@drexel.edu>
2019-07-17 20:55:41 -0400

    Rename

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,35 +1,34 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    const MAX_ISBN_DIGIT_VALUE: u8 = 10;
-
-    let mut digits_cursor = 0;
+    const X: u8 = 10;
+    let mut cursor = 0;
     let mut sum = 0_usize;
 
     for character in isbn.chars() {
         let digit = if character.is_ascii_digit() {
             character as u8 - b'0'
         } else if character == 'X' {
-            let is_check_digit = digits_cursor == MAX_ISBN_DIGIT_VALUE - 1;
+            let is_check_digit = cursor == X - 1;
 
             if !is_check_digit {
                 // X is somewhere before the check digit.
                 return false;
             }
 
-            MAX_ISBN_DIGIT_VALUE
+            X
         } else {
             continue;
         };
 
-        if digits_cursor == MAX_ISBN_DIGIT_VALUE {
+        if cursor == X {
             // Too many digits.
             return false;
         }
 
-        sum += usize::from(digit * (MAX_ISBN_DIGIT_VALUE - digits_cursor));
-        digits_cursor += 1;
+        sum += usize::from(digit * (X - cursor));
+        cursor += 1;
     }
 
-    digits_cursor == MAX_ISBN_DIGIT_VALUE &&
+    cursor == X &&
         sum % 11 == 0
 }
