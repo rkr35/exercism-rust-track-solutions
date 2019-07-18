b595d1787c3386e2c7fdc0d538e0bf389bde61c7
rkr35 <rkr35@drexel.edu>
2019-07-18 14:41:41 -0400

    Add initial implementation

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,31 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    use std::cmp::Ordering::*;
+    use Classification::*;
+
+    if num == 0 {
+        return None;
+    } else if num == 1 {
+        return Some(Deficient);
+    }
+
+    let upper_bound = (num as f64).sqrt() as u64;
+    let mut sum = 1;
+    for i in 2..=upper_bound {
+        if num % i == 0 {
+            sum += i;
+            let other_factor = num / i;
+
+            if other_factor != i {
+                sum += other_factor;
+            }
+        }
+    }
+
+    Some(match sum.cmp(&num) {
+        Equal => Perfect,
+        Greater => Abundant,
+        Less => Deficient
+    })
 }
