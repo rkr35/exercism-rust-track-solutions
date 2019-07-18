e4a25676847412037fc6ef2dd9d96c852ae39ccc
rkr35 <rkr35@drexel.edu>
2019-07-18 16:37:00 -0400

    Add initial files for Hamming

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -8,11 +8,30 @@ pub enum Classification {
 pub fn classify(num: u64) -> Option<Classification> {
     use std::cmp::Ordering::*;
     use Classification::*;
-    match num { 0 => None, 1 => Some(Deficient), _ => {
-        Some(match (2..=(num as f64).sqrt() as u64)
-            .filter_map(|i| if num % i != 0 { None } 
-                            else { let j = num / i; Some((i, if i == j { 0 } else { j })) })
-            .fold(1, |sum, (i, j)| sum + i + j)
-            .cmp(&num) { Equal => Perfect, Greater => Abundant, Less => Deficient })
-    }}
+
+    match num {
+        0 => None,
+        1 => Some(Deficient),
+        _ => Some({
+            let upper_bound = (num as f64).sqrt() as u64;
+            
+            let ordering = (2..=upper_bound)
+                .filter_map(|i| {
+                    if num % i != 0 {
+                        None
+                    } else {
+                        let j = num / i;
+                        Some((i, if i == j { 0 } else { j }))
+                    }
+                })
+                .fold(1, |sum, (i, j)| sum + i + j)
+                .cmp(&num);
+
+            match ordering {
+                Equal => Perfect,
+                Greater => Abundant,
+                Less => Deficient,
+            }
+        }),
+    }
 }
