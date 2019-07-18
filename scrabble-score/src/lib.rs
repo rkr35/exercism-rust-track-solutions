e910bf64826f19ceec242ca3ef77769acab37423
rkr35 <rkr35@drexel.edu>
2019-07-18 17:21:54 -0400

    Add clearer solution

--- scrabble-score/src/lib.rs
+++ scrabble-score/src/lib.rs
@@ -3,22 +3,31 @@
 
 /// Compute the Scrabble score for a word.
 pub fn score(word: &str) -> u64 {
-    const NUM_ENGLISH_DIGITS: usize = 10;
-    const NUM_ENGLISH_LETTERS: usize = 26;
-    const POINTS: [u8; NUM_ENGLISH_LETTERS] = [
-        1,3,3,2,  // abcd
-        1,4,2,4,  // efgh
-        1,8,5,1,  // ijkl
-        3,1,1,3,  // mnop
-        10,1,1,1, // qrst
-        1,4,4,8,  // uvwx
-        4,10      // yz
-    ];
-
     word
         .chars()
-        .filter_map(|character| character
-            .to_digit((POINTS.len() + NUM_ENGLISH_DIGITS) as u32)
-            .map(|digit| u64::from(POINTS[digit as usize - NUM_ENGLISH_DIGITS])))
+        .map(|character| match character.to_ascii_lowercase() {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' 
+                => 1,
+
+            'd' | 'g' 
+                => 2,
+
+            'b' | 'c' | 'm' | 'p'
+                => 3,
+
+            'f' | 'h' | 'v' | 'w' | 'y' 
+                => 4,
+
+            'k' 
+                => 5,
+
+            'j' | 'x'
+                => 8,
+
+            'q' | 'z'
+                => 10,
+            
+            _ => 0,
+        })
         .sum()
 }
