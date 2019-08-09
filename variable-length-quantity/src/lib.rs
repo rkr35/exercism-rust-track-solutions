191c5754f1d61c483ff9811bf4da67fb44e28913
rkr35 <rkr35@drexel.edu>
2019-08-08 20:53:38 -0400

    Add decode implementation

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -1,34 +1,38 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     IncompleteNumber,
     Overflow,
 }
 
-// The type of all input values.
+// The type of all input decoded values.
 type Value = u32;
 
 // The type of all output encoded bytes.
 type Byte = u8;
 
+// The number of significants bits per encoded byte.
+const GROUP_SIZE: Value = 7;
+
+// A mask containing the position of the continuation bit.
+// Used to set the continuation bit for non-terminal encoded bytes.
+const CONTINUE_MASK: Value = 1 << GROUP_SIZE;
+
+// A mask containing the positions of the significant bits.
+// Used to extract a GROUP_SIZE chunk of information from the input
+// to place into an encoded byte, or to extract a chunk of information from an
+// encoded byte to place into a decoded byte.
+const GROUP_MASK: Value = CONTINUE_MASK - 1;
+
+// The number of bits per input value.
+// Used to determine how many groups of GROUP_SIZE the final encoding
+// should have for each input value.
+const BITS_PER_VALUE: Value = (0 as Value).count_zeros();
+
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[Value]) -> Vec<Byte> {
-    // The number of significants bits per encoded byte.
-    const GROUP_SIZE: Value = 7;
-
-    // A mask containing the position of the continuation bit.
-    // Used to set the continuation bit for non-terminal encoded bytes.
-    const CONTINUE_MASK: Value = (1 << GROUP_SIZE);
-
-    // A mask containing the positions of the significant bits.
-    // Used to extract a GROUP_SIZE chunk of information from the input
-    // to place into an encoded byte.
-    const GROUP_MASK: Value = CONTINUE_MASK - 1;
-
-    // The number of bits per input value.
-    // Used to determine how many groups of GROUP_SIZE the final encoding
-    // should have for each input value.
-    const BITS_PER_VALUE: Value = (0 as Value).count_zeros();
-
     values
         .iter()
         .flat_map(|&number| {
@@ -47,7 +51,11 @@ pub fn to_bytes(values: &[Value]) -> Vec<Byte> {
             };
 
             // A helper fuction that extracts the i'th group of GROUP_SIZE bits from `number`.
-            let get_group = move |i| (number >> (i * GROUP_SIZE) & GROUP_MASK) as Byte;
+            let get_group = move |i| {
+                let group = number >> (i * GROUP_SIZE);
+                let significant_bits = group & GROUP_MASK;
+                significant_bits as Byte
+            };
 
             // First encode, from right-to-left, all the groups that need a continuation
             // bit set.
@@ -63,5 +71,58 @@ pub fn to_bytes(values: &[Value]) -> Vec<Byte> {
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
+    // Holds the numbers we decode from the byte stream.
+    let mut decoded_numbers = vec![];
+
+    // Used to hold results of decoding a single number.
+    // Variant `Some` when the accumulator is currently being used to decode a number, i.e.,
+    // the below algorithm has not reached the terminal byte.
+    // Variant `None` when not decoding a number.
+    let mut accumulator = None;
+
+    for byte in bytes {
+        let (is_terminal, decoded_number_so_far) = {
+            // Ensure the accumulator is initialized.
+            let accumulator = accumulator.get_or_insert(0 as Value);
+            
+            // Extract a piece of the encoded number.
+            let payload = byte & GROUP_MASK as Byte;
+
+            // Make room in the accumulator for that piece by shifting left GROUP_SIZE bits.
+            // We shift left by multiplying by 2^GROUP_SIZE so that we can use checked_mul()
+            // to check for an overflow.
+            *accumulator = (*accumulator)
+                .checked_mul(CONTINUE_MASK)
+                .ok_or(Error::Overflow)?;
+
+            // Add that piece to the accumulator.
+            *accumulator |= u32::from(payload);
+
+            // We know the sign-bit (the leftmost bit) will be `1` if
+            // we are expecting more bytes and `0` if we are done decoding
+            // this number, i.e., we reached the "terminal" byte.
+            let sign_bit = byte >> GROUP_SIZE;
+            let is_terminal = sign_bit == 0;
+
+            (is_terminal, *accumulator)
+        };
+
+        // Check if we are expecting more pieces for this encoded number.
+        if is_terminal {
+            // If not, then we fully decoded the number. 
+            // We add this decoded number to our collection.
+            decoded_numbers.push(decoded_number_so_far);
+
+            // We reset the accumulator to the `None` variant to signify that we are done
+            // using it for decoding this number.
+            accumulator = None;
+        }
+    }
+
+    // Did we stop in the middle of decoding a number?
+    if accumulator.is_some() {
+        Err(Error::IncompleteNumber)
+    } else {
+        Ok(decoded_numbers)
+    }
 }
