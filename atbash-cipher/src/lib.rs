0e6d8bb7e1c8c904471245c8e78e32120d47b98a
rkr35 <rkr35@drexel.edu>
2019-08-17 15:54:50 -0400

    Format

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,49 +1,23 @@
-/// "Encipher" with the Atbash cipher.
 fn encode_using_group_size(plain: &str, group_size: Option<usize>) -> String {
     let group_size = group_size.unwrap_or(std::usize::MAX);
 
     let mut s = plain
         .bytes()
-        .filter_map(|c| if c.is_ascii_alphabetic() {
-            // Map characters to their partner on the opposite end of the
-            // alphabet.
-            Some(char::from(b'a' + b'z' - c.to_ascii_lowercase()))
-        } else if c.is_ascii_digit() {
-            // Pass digits untouched.
-            Some(char::from(c))
-        } else {
-            // Ignore all other character classes.
-            None
-        })
+        .filter_map(|c| 
+            if      c.is_ascii_alphabetic() { Some(char::from(b'a' + b'z' - c.to_ascii_lowercase())) } 
+            else if c.is_ascii_digit()      { Some(char::from(c)) } 
+            else                            { None }
+        )
         .enumerate()
         .fold(String::with_capacity(plain.len()), |mut encoded, (i, c)| {
             encoded.push(c);
-
-            // If we're at the end of a group...
-            if i % group_size == (group_size - 1) {
-                // ...then append a space.
-                encoded += " ";
-            }
-
+            if i % group_size == (group_size - 1) { encoded += " "; }
             encoded
         });
 
-    if let Some(last) = s.pop() {
-        // If we have an extraneous space, then remove it.
-        // Otherwise, restore the character we popped.
-        if last != ' ' {
-            s.push(last)
-        }
-    }
-
+    if let Some(last) = s.pop() { if last != ' ' { s.push(last) } }
     s
 }
 
-pub fn encode(plain: &str) -> String {
-    encode_using_group_size(plain, Some(5))
-}
-
-/// "Decipher" with the Atbash cipher.
-pub fn decode(cipher: &str) -> String {
-    encode_using_group_size(cipher, None)
-}
+pub fn encode(plain: &str) -> String    { encode_using_group_size(plain, Some(5)) }
+pub fn decode(cipher: &str) -> String   { encode_using_group_size(cipher, None) }
