4a93f7904aa7642072e9c37608274344d80825a2
rkr35 <rkr35@drexel.edu>
2019-08-17 10:07:25 -0400

    Collapse rules 2, 3, and 4 into one regex

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -6,25 +6,17 @@ pub fn translate(input: &str) -> String {
     fn translate_word(word: &str) -> String {
         lazy_static! {
             static ref RULE_1: Regex = Regex::new(r"^(?:[aeiou]|xr|yt)\w*$").unwrap();
-            static ref RULE_2: Regex = Regex::new(r"^(?P<consonant>[^aeiou]+)(?P<rest>\w*)$").unwrap();
-            static ref RULE_3: Regex = Regex::new(r"^(?P<consonant>[^aeiou]*)qu(?P<rest>\w*)$").unwrap();
-            static ref RULE_4: Regex = Regex::new(r"^(?P<consonant>[^aeiou]+)y(?P<rest>\w*)$").unwrap();
+            static ref RULES_2_3_4: Regex = Regex::new(r"^(y)?([^\saeiouqy]*)(qu)?(q)?(\w+)$").unwrap();
         }
 
         let word = word.to_string();
 
         if RULE_1.is_match(&word) {
-            return word + "ay";
-        }
-
-        if let Owned(replaced) = RULE_4.replace(&word, "y${rest}${consonant}ay") {
-            replaced
-        } else if let Owned(replaced) = RULE_3.replace(&word, "${rest}${consonant}quay") {
-            replaced
-        } else if let Owned(replaced) = RULE_2.replace(&word, "${rest}${consonant}ay") {
+            word + "ay"
+        } else if let Owned(replaced) = RULES_2_3_4.replace(&word, "${5}${2}${3}${4}${1}ay") {
             replaced
         } else {
-            unreachable!("Encountered an word that does not match any of the rules: {}", word);
+            unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word);
         }
     }
 
