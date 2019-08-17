793506aad7c1e9a394bbd839150088d36d0d6dad
rkr35 <rkr35@drexel.edu>
2019-08-17 10:30:30 -0400

    Collect directly into Strnig

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,21 +1,23 @@
 pub fn translate(input: &str) -> String {
-    input
+    let mut s: String = input
         .split(' ')
         .map(|word| {
             use regex::Regex;
             use std::borrow::Cow::Owned;
-            // todo: Use lazy_static! to cache regular expressions.
+
             const RULES_2_3_4: &str = r"^(y)?([^\saeiouqy]*)(qu)?(q)?(\w+)$";
             fn r(pattern: &str) -> Regex { Regex::new(pattern).unwrap() }
 
             if r(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
-                word.to_owned() + "ay"
-            } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay") {
+                word.to_owned() + "ay "
+            } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay ") {
                 replaced
             } else {
                 unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word)
             }
         })
-        .collect::<Vec<_>>()
-        .join(" ")
+        .collect();
+
+    s.pop();
+    s
 }
