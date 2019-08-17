5a2a193458ef9eb6879287b5d28f7e4922d8b079
rkr35 <rkr35@drexel.edu>
2019-08-17 10:49:55 -0400

    Use lazy_static to cache regular expressions

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,16 +1,18 @@
 pub fn translate(input: &str) -> String {
+    use regex::Regex;
+    
+    lazy_static::lazy_static! {
+        static ref RULE_1: Regex = Regex::new(r"^(?:[aeiou]|xr|yt)\w*$").unwrap();
+        static ref RULES_2_3_4: Regex = Regex::new(r"^(y)?([^aeiouqy]*)(qu)?(q)?(\w+)$").unwrap();
+    }
+
     let mut s: String = input
         .split(' ')
         .map(|word| {
-            use regex::Regex;
             use std::borrow::Cow::Owned;
-
-            const RULES_2_3_4: &str = r"^(y)?([^aeiouqy]*)(qu)?(q)?(\w+)$";
-            fn r(pattern: &str) -> Regex { Regex::new(pattern).unwrap() }
-
-            if r(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
+            if RULE_1.is_match(&word) {
                 word.to_owned() + "ay "
-            } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay ") {
+            } else if let Owned(replaced) = RULES_2_3_4.replace(&word, "${5}${2}${3}${4}${1}ay ") {
                 replaced
             } else {
                 unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word)
