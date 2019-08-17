f4285d6bd9033e5030404dc07f21d7f2eed0878e
rkr35 <rkr35@drexel.edu>
2019-08-17 10:20:18 -0400

    Replace const fn closure with inline fn

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -5,11 +5,11 @@ pub fn translate(input: &str) -> String {
     fn translate_word(word: &str) -> String {
         // todo: Use lazy_static! to cache constructed regular expressions.
         const RULES_2_3_4: &str = r"^(y)?([^\saeiouqy]*)(qu)?(q)?(\w+)$";
-        const R: fn(&str) -> Regex = |pattern| Regex::new(pattern).unwrap();
+        fn r(pattern: &str) -> Regex { Regex::new(pattern).unwrap() }
 
-        if R(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
+        if r(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
             word.to_owned() + "ay"
-        } else if let Owned(replaced) = R(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay") {
+        } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay") {
             replaced
         } else {
             unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word);
