3d0e1ac77ccd45916b8f0886f511af1b2bf53f96
rkr35 <rkr35@drexel.edu>
2019-08-17 10:22:32 -0400

    Inline translate_word()

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,24 +1,21 @@
 pub fn translate(input: &str) -> String {
-    use regex::Regex;
-    use std::borrow::Cow::Owned;
-
-    fn translate_word(word: &str) -> String {
-        // todo: Use lazy_static! to cache constructed regular expressions.
-        const RULES_2_3_4: &str = r"^(y)?([^\saeiouqy]*)(qu)?(q)?(\w+)$";
-        fn r(pattern: &str) -> Regex { Regex::new(pattern).unwrap() }
-
-        if r(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
-            word.to_owned() + "ay"
-        } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay") {
-            replaced
-        } else {
-            unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word);
-        }
-    }
-
     input
         .split_ascii_whitespace()
-        .map(translate_word)
+        .map(|word| {
+            use regex::Regex;
+            use std::borrow::Cow::Owned;
+            // todo: Use lazy_static! to cache constructed regular expressions.
+            const RULES_2_3_4: &str = r"^(y)?([^\saeiouqy]*)(qu)?(q)?(\w+)$";
+            fn r(pattern: &str) -> Regex { Regex::new(pattern).unwrap() }
+
+            if r(r"^(?:[aeiou]|xr|yt)\w*$").is_match(&word) {
+                word.to_owned() + "ay"
+            } else if let Owned(replaced) = r(RULES_2_3_4).replace(&word, "${5}${2}${3}${4}${1}ay") {
+                replaced
+            } else {
+                unreachable!("Encountered a word that does not match any of the rules: \"{}\"", word)
+            }
+        })
         .collect::<Vec<_>>()
         .join(" ")
 }
