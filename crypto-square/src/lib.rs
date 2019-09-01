1c934d891b5215c413f86bd70b969feb74e9cec2
rkr35 <rkr35@drexel.edu>
2019-09-01 11:34:15 -0400

    Add iterator implementation

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -1,44 +1,26 @@
 #![warn(clippy::pedantic)]
 
 pub fn encrypt(input: &str) -> String {
-    fn normalize(c: char) -> Option<char> {
-        if c.is_ascii_alphanumeric() {
-            Some(c.to_ascii_lowercase())
-        } else {
-            None
-        }
-    }
-
     let normalized_letters: Vec<_> = input
         .chars()
-        .filter_map(normalize)
+        .filter_map(|c| {
+            if c.is_ascii_alphanumeric() {
+                Some(c.to_ascii_lowercase())
+            } else {
+                None
+            }
+        })
         .collect();
 
-    let [rows, columns] = {
-        let number_letters = normalized_letters.len() as f64;
-        let r = number_letters.sqrt().floor();
-        let c = (number_letters / r).ceil();
-        [r as usize, c as usize]
-    };
+    let columns = (normalized_letters.len() as f64).sqrt().ceil() as usize;
 
-    let mut encrypted = String::with_capacity(columns * (rows + 1));
-
-    for c in 0..columns {
-        for r in 0..rows {
-            let position = r * columns + c;
-
-            let letter = normalized_letters
-                .get(position)
-                .copied()
-                .unwrap_or(' ');
-
-            encrypted.push(letter);
-        }
-
-        if c != columns - 1 {
-            encrypted.push(' ');
-        }
-    }
-    
-    encrypted
+    (0..columns)
+        .map(|c|
+            normalized_letters
+                .chunks(columns)
+                .map(|row| row.get(c).copied().unwrap_or(' '))
+                .collect::<String>()
+        )
+        .collect::<Vec<_>>()
+        .join(" ")
 }
