1f41e8adfb1385229cfa01c9aaac77045e021745
rkr35 <rkr35@drexel.edu>
2019-09-02 11:31:42 -0400

    Add implementation

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    input
+        .chars()
+        .map(|c| if c.is_alphabetic() {
+            let first = if c.is_ascii_uppercase() { b'A' } else { b'a' } as i8;
+            let negative_mod = |n, m| ((n % m) + m) % m;
+            let old_position = c as i8 - first;
+            let new_position = negative_mod(old_position + key, 26);
+            (first + new_position) as u8 as char
+        } else {
+            c
+        })
+        .collect()  
 }
