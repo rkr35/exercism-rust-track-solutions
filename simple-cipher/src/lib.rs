51820a80ab853d200f56ce1aa1fd1a34a8615d78
rkr35 <rkr35@drexel.edu>
2019-10-22 20:30:19 -0400

    Add working implementation of encode()

--- simple-cipher/src/lib.rs
+++ simple-cipher/src/lib.rs
@@ -1,5 +1,32 @@
+#![warn(clippy::pedantic)]
+
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    unimplemented!("Use {} to encode {} using shift cipher", key, s)
+    if key.is_empty() {
+        return None;
+    }
+    
+    let mut encoded = String::with_capacity(s.len());
+
+    let s = s
+        .bytes()
+        .zip(key.bytes().cycle());
+
+    for (s, k) in s {
+        const A: u8 = b'a';
+
+        if !s.is_ascii_lowercase() || !k.is_ascii_lowercase() {
+            return None;
+        }
+
+        let s = s - A; 
+        let k = k - A; 
+        let s = (s + k) % 26;
+        let s = s + A;
+
+        encoded.push(s.into());
+    }
+
+    Some(encoded)
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
