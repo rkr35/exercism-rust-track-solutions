2300dd97c55640ad891a7880c2000fce67c72aaf
rkr35 <rkr35@drexel.edu>
2019-10-23 20:05:12 -0400

    cargo fmt

--- simple-cipher/src/lib.rs
+++ simple-cipher/src/lib.rs
@@ -2,19 +2,22 @@
 
 const A: u8 = b'a';
 
-pub fn code<F>(key: &str, s: &str, mut coder: F) -> Option<String> 
-where F: FnMut(u8, u8) -> u8 {
+pub fn code<F>(key: &str, s: &str, mut coder: F) -> Option<String>
+where
+    F: FnMut(u8, u8) -> u8,
+{
     if key.is_empty() {
         return None;
     }
-    
-    s
-        .bytes()
+
+    s.bytes()
         .zip(key.bytes().cycle())
-        .map(|(c, k)| if c.is_ascii_lowercase() && k.is_ascii_lowercase() {
-            Some((A + coder(c, k) % 26) as char)
-        } else {
-            None
+        .map(|(c, k)| {
+            if c.is_ascii_lowercase() && k.is_ascii_lowercase() {
+                Some((A + coder(c, k) % 26) as char)
+            } else {
+                None
+            }
         })
         .collect()
 }
