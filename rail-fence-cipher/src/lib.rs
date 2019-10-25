93fee2456f7bccf1af6f5162bf933f20709de9d6
rkr35 <rkr35@drexel.edu>
2019-10-24 20:20:37 -0400

    Add implementation of encode

--- rail-fence-cipher/src/lib.rs
+++ rail-fence-cipher/src/lib.rs
@@ -1,12 +1,39 @@
-pub struct RailFence;
+#![warn(clippy::pedantic)]
+
+pub struct RailFence {
+    num_rails: usize
+}
 
 impl RailFence {
-    pub fn new(rails: u32) -> RailFence {
-        unimplemented!("Construct a new fence with {} rails", rails)
+    pub fn new(rails: u32) -> Self {
+        Self {
+            num_rails: rails as usize,
+        }
+    }
+
+    fn rails_iter(&self, length: usize) -> impl Iterator<Item = usize> {
+        let r = self.num_rails;
+
+        (0..r)
+            .chain((1..r-1).rev())
+            .cycle()
+            .zip(0..)
+            .map(move |(y, x)| y * length + x)
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {}", text)
+        let n = text.len();
+
+        let mut encoded = vec![None; n * self.num_rails];
+        
+        self.rails_iter(n)
+            .zip(text.chars())
+            .for_each(|(rail, ch)| encoded[rail] = Some(ch));
+
+        encoded
+            .into_iter()
+            .filter_map(|c| c)
+            .collect()
     }
 
     pub fn decode(&self, cipher: &str) -> String {
