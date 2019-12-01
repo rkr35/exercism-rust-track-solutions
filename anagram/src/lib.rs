22b993cf2344258f5d48bb4e8a982a8aa7c0f1da
rkr35 <rkr35@drexel.edu>
2019-11-30 20:25:30 -0500

    Add working implementation of anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,25 @@
+#![warn(clippy::pedantic)]
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+
+    let word_sorted = {
+        let mut w: Vec<_> = word.clone().chars().collect();
+        w.sort_unstable();
+        w
+    };
+
+    possible_anagrams
+        .iter()
+        .copied()
+        .filter(|possible| {
+            let possible = possible.to_lowercase();
+            possible.len() == word.len() && word != possible && {
+                let mut possible: Vec<_> = possible.chars().collect();
+                possible.sort_unstable();
+                possible == word_sorted                
+            }
+        })
+        .collect()
 }
