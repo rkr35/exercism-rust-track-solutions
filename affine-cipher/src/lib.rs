6db051352c10560732a5370b424c9ad485d13599
rkr35 <rkr35@drexel.edu>
2019-12-26 16:09:33 -0500

    Reformat

--- affine-cipher/src/lib.rs
+++ affine-cipher/src/lib.rs
@@ -1,3 +1,5 @@
+use core::iter::{FromIterator, IntoIterator};
+
 /// While the problem description indicates a return status of 1 should be returned on errors,
 /// it is much more common to return a `Result`, so we provide an error type for the result here.
 #[derive(Debug, Eq, PartialEq)]
@@ -6,6 +8,37 @@ pub enum AffineCipherError {
 }
 
 const ALPHABET_SIZE: i32 = 26;
+
+/// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
+/// returning a return code, the more common convention in Rust is to return a `Result`.
+pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
+    use core::convert::TryInto;
+
+    if !is_coprime(a, ALPHABET_SIZE) {
+        return Err(AffineCipherError::NotCoprime(a));
+    }
+
+    Ok(plaintext
+        .bytes()
+        .filter_map(|c| match c {
+            c if c.is_ascii_digit() => Some(c),
+            c if c.is_ascii_alphabetic() => {
+                let x = i32::from(c.to_ascii_lowercase() - b'a');
+                let e = (a*x + b) % ALPHABET_SIZE;
+                let e: u8 = e.try_into().expect("Encountered a non-ASCII alphabetic character");
+                Some(b'a' + e)
+            },
+            _ => None,
+        })
+        .collect::<Encoded>()
+        .into())
+}
+
+/// Decodes the ciphertext using the affine cipher with key (`a`, `b`). Note that, rather than
+/// returning a return code, the more common convention in Rust is to return a `Result`.
+pub fn decode(ciphertext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
+    unimplemented!("Decode {} with the key ({}, {})", ciphertext, a, b);
+}
 
 fn gcd(mut a: i32, mut b: i32) -> i32 {
     while b != 0 {
@@ -21,46 +54,46 @@ fn is_coprime(a: i32, b: i32) -> bool {
     gcd(a, b) == 1
 }
 
-/// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
-pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
-    use core::convert::TryInto;
+#[derive(Default)]
+struct Encoded {
+    encoded: String,
+    num_added: usize,
+}
 
-    if !is_coprime(a, ALPHABET_SIZE) {
-        return Err(AffineCipherError::NotCoprime(a));
+impl Encoded {
+    const GROUP_SIZE: usize = 5;
+
+    fn push(&mut self, c: u8) {
+        self.add_space();
+        self.encoded.push(c.into());
+        self.num_added += 1;
     }
 
-    let mut encoded = String::new();
-    let mut num_added = 0;
-
-    for c in plaintext.bytes() {
-        if c.is_ascii_digit() {
-            if num_added != 0 && num_added % 5 == 0 {
-                encoded.push(' ');
-            }
-
-            encoded.push(c.into());
-            num_added += 1;
-        } else if c.is_ascii_alphabetic() {
-            // E(x) = (ax + b) mod m
-            let x = i32::from(c.to_ascii_lowercase() - b'a');
-
-            let e: u8 = ((a*x + b) % ALPHABET_SIZE).try_into().expect("Encountered a non-ASCII alphabetic character");
-            
-            if num_added != 0 && num_added % 5 == 0 {
-                encoded.push(' ');
-            }
-
-            encoded.push((b'a' + e).into());
-            num_added += 1;
+    fn add_space(&mut self) {
+        if self.needs_space() {
+            self.encoded.push(' ');
         }
     }
 
-    Ok(encoded)
+    fn needs_space(&self) -> bool {
+        self.num_added > 0 && self.num_added % Self::GROUP_SIZE == 0
+    }
 }
 
-/// Decodes the ciphertext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
-pub fn decode(ciphertext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
-    unimplemented!("Decode {} with the key ({}, {})", ciphertext, a, b);
+impl FromIterator<u8> for Encoded {
+    fn from_iter<I: IntoIterator<Item=u8>>(iter: I) -> Self {
+        let mut encoded = Self::default();
+
+        for c in iter {
+            encoded.push(c);
+        }
+
+        encoded
+    }
+}
+
+impl Into<String> for Encoded {
+    fn into(self) -> String {
+        self.encoded
+    }
 }
