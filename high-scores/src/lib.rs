22492a6bbf7b351725577b2c23ccabd8ce94421d
rkr35 <rkr35@drexel.edu>
2019-12-26 13:56:00 -0500

    Add O(scores.len()) no-sort solution

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -19,13 +19,44 @@ impl<'sc> HighScores<'sc> {
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        self.personal_top_three().first().copied()
+        self.scores().iter().max().copied()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let mut sorted = self.scores().to_vec();
-        sorted.sort_unstable_by(|a, b| b.cmp(a));
-        sorted.truncate(3);
-        sorted
+        const N: usize = 3;
+
+        // Our Vec will hold at most N + 1 elements at a time.
+        // The `+1` is for when we have a full Vec of N elements, and we insert
+        // into the collection to get N + 1 elements before truncating back to N
+        // elements.
+        let mut top = Vec::with_capacity(N + 1);
+
+        // Iterate over all our scores.
+        for &score in self.scores {
+
+            // Iterate over our top scores.
+            for i in 0..N {
+
+                // Is the current score greater than this top score?
+                if score >= top.get(i).copied().unwrap_or(0) {
+
+                    // Place the current score into the position formerly occupied
+                    // by that top score.
+                    top.insert(i, score);
+
+                    // Snip off any excess element that got pushed right.
+                    // We want to truncate per update because the cost of inserting into the collection
+                    // is O(top.len()). If we truncated only once outside the loops before returning, 
+                    // then we're needlessly paying the cost of shifting to the right elements we don't care about.
+                    top.truncate(N);
+
+                    // Since we just added the current score into `top`, there's nothing more for us to do with this
+                    // current score.
+                    break;
+                }
+            }
+        }
+
+        top
     }
 }
