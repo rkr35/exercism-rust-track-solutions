0a0d2e838c280c2ee23f22aaf37fd73f7401af5c
rkr35 <rkr35@drexel.edu>
2020-02-14 20:14:22 -0500

    Add implementation

--- ocr-numbers/src/lib.rs
+++ ocr-numbers/src/lib.rs
@@ -1,12 +1,121 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidRowCount(usize),
     InvalidColumnCount(usize),
 }
 
+fn read(top: &str, middle: &str, bottom: &str) -> &'static str {
+    match [top, middle, bottom] {
+        [
+            " _ ",
+            "| |",
+            "|_|",
+        ] => "0",
+
+        [
+            "   ",
+            "  |",
+            "  |",
+        ] => "1",
+
+        [
+            " _ ",
+            " _|",
+            "|_ ",
+        ] => "2",
+
+        [
+            " _ ",
+            " _|",
+            " _|",
+        ] => "3",
+
+        [
+            "   ",
+            "|_|",
+            "  |",
+        ] => "4",
+
+        [
+            " _ ",
+            "|_ ",
+            " _|",
+        ] => "5",
+
+        [
+            " _ ",
+            "|_ ",
+            "|_|",
+        ] => "6",
+
+        [
+            " _ ",
+            "  |",
+            "  |",
+        ] => "7",
+
+        [
+            " _ ",
+            "|_|",
+            "|_|",
+        ] => "8",
+
+        [
+            " _ ",
+            "|_|",
+            " _|",
+        ] => "9",
+
+        _ => "?",
+    }
+}
+
 pub fn convert(input: &str) -> Result<String, Error> {
-    unimplemented!("Convert the input '{}' to a string", input);
+    const DIGIT_WIDTH: usize = 3;
+    const DIGIT_HEIGHT: usize = 4;
+
+    let lines: Vec<_> = input.split('\n').collect();
+    
+    if lines.is_empty() {
+        return Ok("".into());
+    }
+
+    let num_rows = lines.len();
+
+    if num_rows % DIGIT_HEIGHT != 0 {
+        return Err(Error::InvalidRowCount(num_rows));
+    }
+
+    let bad_num_columns = lines
+        .iter()
+        .map(|l| l.len())
+        .find(|c| c % DIGIT_WIDTH != 0);
+
+    if let Some(c) = bad_num_columns {
+        return Err(Error::InvalidColumnCount(c));
+    }
+    
+    let num_digits = lines[0].len() / DIGIT_WIDTH;
+
+    let mut s: String = lines
+        .chunks_exact(DIGIT_HEIGHT)
+        .flat_map(|chunk|
+            (0..num_digits)
+                .map(move |i| {
+                    let start = i * DIGIT_WIDTH;
+                    let end = (i + 1) * DIGIT_WIDTH;
+
+                    let top = &chunk[0][start..end];
+                    let middle = &chunk[1][start..end];
+                    let bottom = &chunk[2][start..end];
+
+                    read(top, middle, bottom)
+                })
+                .chain(std::iter::once(","))
+        )
+        .collect();
+
+    s.pop();
+
+    Ok(s)
 }
