d5d63ab62116d33b188a8ffa25cc9e432f912e7d
rkr35 <rkr35@drexel.edu>
2020-02-17 15:01:48 -0500

    Wordy: Add initial implementation

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -1,8 +1,58 @@
 pub struct WordProblem;
 
 pub fn answer(command: &str) -> Option<i32> {
-    unimplemented!(
-        "Return the result of the command '{}' or None, if the command is invalid.",
-        command
-    );
+    enum Op {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    }
+
+    use Op::*;
+
+    let tokens = command.split(' ');
+    let mut number = None;
+    let mut op = None;
+
+    for token in tokens {
+        if let Some(o) = &mut op {
+            match token {
+                "plus" => return None,
+                "minus" => return None,
+                "multiplied" => return None,
+                "divided" => return None,
+                _ => (),
+            };
+
+            if let Ok(n) = token.trim_end_matches('?').parse::<i32>() {
+                match o {
+                    Add => number = Some(number.unwrap() + n),
+                    Subtract => number = Some(number.unwrap() - n),
+                    Multiply => number = Some(number.unwrap() * n),
+                    Divide => number = Some(number.unwrap() / n),
+                }
+                op = None;
+            }
+        }
+        else if number.is_some() {
+            match token {
+                "plus" => op = Some(Add),
+                "minus" => op = Some(Subtract),
+                "multiplied" => op = Some(Multiply),
+                "divided" => op = Some(Divide),
+                _ if token.ends_with('?') => return None,
+                _ => (),
+            }
+        } else if let Ok(n) = token.trim_end_matches('?').parse::<i32>() {
+            number = Some(n);
+        } else {
+            continue;
+        }
+    }
+
+    if op.is_some() {
+        None
+    } else {
+        number
+    }
 }
