edd2d82324efc39f01469367d901d8ec296178b0
rkr35 <rkr35@drexel.edu>
2020-02-17 18:12:41 -0500

    Replace 0-length check with .is_empty()

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -9,7 +9,7 @@ pub enum Comparison {
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     use Comparison::*;
     let sub = |l: &[T], s: &[T]| l
-        .windows(if s.len() == 0 { return true; } else { s.len() })
+        .windows(if s.is_empty() { return true; } else { s.len() })
         .any(|w| w == s);
     match [first_list.len(), second_list.len()] {
         [f, s] if f == s => if first_list == second_list { Equal } else { Unequal },
