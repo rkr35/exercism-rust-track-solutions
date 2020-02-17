18c8499c250f0a77cf43c405d05a5575d23b0ad0
rkr35 <rkr35@drexel.edu>
2020-02-17 15:44:23 -0500

    Add comments

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -1,3 +1,49 @@
+pub fn answer(command: &str) -> Option<i32> {
+    let mut number = None;
+    let mut op: Option<Op> = None;
+
+    for token in command.split(' ') {
+        if let Some(o) = &op {
+            // Look for second operand.
+
+            if Op::from(token).is_some() {
+                // Found another operation in the middle of an operation.
+                // So the input is malformed.
+                return None;
+            }
+
+            if let Some(m) = parse(token) {
+                // Found second operand. Update accumulator.
+                number = Some(o.calc(number.unwrap(), m));
+
+                // Reset operation so we can begin a new operation.
+                op = None;
+            }
+        }
+        else if number.is_some() {
+            // Look for operation.
+
+            if token != "power?" && token.ends_with('?') {
+                // Input ended before operating on accumulator.
+                // So the input is malformed.
+                return None;
+            }
+            op = Op::from(token);
+        } else {
+            // Look for first operand.
+            number = parse(token);
+        }
+    }
+
+    if op.is_some() {
+        // Input ended before completing current operation.
+        // So the input is malformed.
+        None
+    } else {
+        number
+    }
+}
+
 enum Op {
     Add,
     Subtract,
@@ -36,36 +82,4 @@ fn parse(token: &str) -> Option<i32> {
     ["?", "st", "nd", "rd", "th"]
         .iter()
         .find_map(|s| token.trim_end_matches(s).parse().ok())
-}
-
-pub fn answer(command: &str) -> Option<i32> {
-    let mut number = None;
-    let mut op: Option<Op> = None;
-
-    for token in command.split(' ') {
-        if let Some(o) = &op {
-            if Op::from(token).is_some() {
-                return None;
-            }
-
-            if let Some(m) = parse(token) {
-                number = Some(o.calc(number.unwrap(), m));
-                op = None;
-            }
-        }
-        else if number.is_some() {
-            if token != "power?" && token.ends_with('?') {
-                return None;
-            }
-            op = Op::from(token);
-        } else {
-            number = parse(token);
-        }
-    }
-
-    if op.is_some() {
-        None
-    } else {
-        number
-    }
 }
