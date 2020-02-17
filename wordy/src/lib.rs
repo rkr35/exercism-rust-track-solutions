afcfd4d26a50b15d87b98a9f89ef01e70d8e0353
rkr35 <rkr35@drexel.edu>
2020-02-17 15:13:28 -0500

    Refactor Op

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -1,48 +1,55 @@
 pub struct WordProblem;
 
-pub fn answer(command: &str) -> Option<i32> {
-    enum Op {
-        Add,
-        Subtract,
-        Multiply,
-        Divide,
+enum Op {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+impl Op {
+    fn from(s: &str) -> Option<Self> {
+        use Op::*;
+        Some(match s {
+            "plus" => Add,
+            "minus" => Subtract,
+            "multiplied" => Multiply,
+            "divided" => Divide,
+            _ => return None,
+        })
     }
 
-    use Op::*;
+    fn calc(&self, a: i32, b: i32) -> i32 {
+        use Op::*;
+        match self {
+            Add => a + b,
+            Subtract => a - b,
+            Multiply => a * b,
+            Divide => a / b,
+        }
+    }
+}
 
-    let tokens = command.split(' ');
+pub fn answer(command: &str) -> Option<i32> {
     let mut number = None;
-    let mut op = None;
+    let mut op: Option<Op> = None;
 
-    for token in tokens {
+    for token in command.split(' ') {
         if let Some(o) = &mut op {
-            match token {
-                "plus" => return None,
-                "minus" => return None,
-                "multiplied" => return None,
-                "divided" => return None,
-                _ => (),
-            };
+            if Op::from(token).is_some() {
+                return None;
+            }
 
-            if let Ok(n) = token.trim_end_matches('?').parse::<i32>() {
-                match o {
-                    Add => number = Some(number.unwrap() + n),
-                    Subtract => number = Some(number.unwrap() - n),
-                    Multiply => number = Some(number.unwrap() * n),
-                    Divide => number = Some(number.unwrap() / n),
-                }
+            if let Ok(m) = token.trim_end_matches('?').parse::<i32>() {
+                number = Some(o.calc(number.unwrap(), m));
                 op = None;
             }
         }
         else if number.is_some() {
-            match token {
-                "plus" => op = Some(Add),
-                "minus" => op = Some(Subtract),
-                "multiplied" => op = Some(Multiply),
-                "divided" => op = Some(Divide),
-                _ if token.ends_with('?') => return None,
-                _ => (),
+            if token.ends_with('?') {
+                return None;
             }
+            op = Op::from(token);
         } else if let Ok(n) = token.trim_end_matches('?').parse::<i32>() {
             number = Some(n);
         } else {
