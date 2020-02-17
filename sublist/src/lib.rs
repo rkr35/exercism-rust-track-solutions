24a3ded6ca7260a7c1b20f3be994a4ca6ea80fcb
rkr35 <rkr35@drexel.edu>
2020-02-17 18:07:10 -0500

    Refactor to set cases in match arms

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::{Ord, Ordering};
-
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -9,32 +7,15 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    use Ordering::{Less, Greater, Equal as Eq};
     use Comparison::*;
+    let sublist = |larger: &[T], smaller: &[T]| larger
+        .windows(if smaller.len() == 0 { return true; } else { smaller.len() })
+        .any(|w| w == smaller);
 
-    let len_cmp = first_list
-        .len()
-        .cmp(&second_list.len());
-
-    let [smaller, larger] = if let Greater = len_cmp {
-        [second_list, first_list]
-    } else {
-        [first_list, second_list]
-    };
-
-    let is_smaller_sublist_of_larger = (0..=larger.len() - smaller.len())
-        .any(|i| larger[i..]
-            .iter()
-            .zip(smaller.iter())
-            .all(|(l, s)| l == s));
-        
-    if is_smaller_sublist_of_larger {
-        match len_cmp {
-            Less => Sublist,
-            Greater => Superlist,
-            Eq => Equal,
-        }
-    } else {
-        Unequal
+    match [first_list.len(), second_list.len()] {
+        [f, s] if f == s => if first_list == second_list { Equal } else { Unequal },
+        [f, s] if f < s => if sublist(second_list, first_list) { Sublist } else { Unequal },
+        [f, s] if f > s => if sublist(first_list, second_list) { Superlist } else { Unequal },
+        [f, s] => unreachable!("|first| = {}, |second| = {}", f, s),
     }
 }
