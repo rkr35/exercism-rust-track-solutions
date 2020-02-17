8e01d445e7b5643b2277e45efded31610cdc8bc5
rkr35 <rkr35@drexel.edu>
2020-02-17 17:36:48 -0500

    Sublist: Add working implementation

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::{Ord, Ordering};
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,6 +8,33 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    use Ordering::{Less, Greater, Equal as Eq};
+    use Comparison::*;
+
+    let len_cmp = first_list
+        .len()
+        .cmp(&second_list.len());
+
+    let [smaller, larger] = if let Greater = len_cmp {
+        [second_list, first_list]
+    } else {
+        [first_list, second_list]
+    };
+
+    let is_smaller_sublist_of_larger = (0..=larger.len() - smaller.len())
+        .any(|i| larger[i..]
+            .iter()
+            .zip(smaller.iter())
+            .all(|(l, s)| l == s));
+        
+    if is_smaller_sublist_of_larger {
+        match len_cmp {
+            Less => Sublist,
+            Greater => Superlist,
+            Eq => Equal,
+        }
+    } else {
+        Unequal
+    }
 }
