0b918caab6f04074946bbad334ecdbc3f0307119
rkr35 <rkr35@drexel.edu>
2020-02-17 18:10:39 -0500

    Shorten names

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -8,14 +8,13 @@ pub enum Comparison {
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     use Comparison::*;
-    let sublist = |larger: &[T], smaller: &[T]| larger
-        .windows(if smaller.len() == 0 { return true; } else { smaller.len() })
-        .any(|w| w == smaller);
-
+    let sub = |l: &[T], s: &[T]| l
+        .windows(if s.len() == 0 { return true; } else { s.len() })
+        .any(|w| w == s);
     match [first_list.len(), second_list.len()] {
         [f, s] if f == s => if first_list == second_list { Equal } else { Unequal },
-        [f, s] if f < s => if sublist(second_list, first_list) { Sublist } else { Unequal },
-        [f, s] if f > s => if sublist(first_list, second_list) { Superlist } else { Unequal },
+        [f, s] if f < s => if sub(second_list, first_list) { Sublist } else { Unequal },
+        [f, s] if f > s => if sub(first_list, second_list) { Superlist } else { Unequal },
         [f, s] => unreachable!("|first| = {}, |second| = {}", f, s),
     }
 }
